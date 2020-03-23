d760f8e18ced2f011f93c49d56620d87b0dff3e0
DarshanDeshpande <39432636+DarshanDeshpande@users.noreply.github.com>
2020-03-23 12:57:32 +0530

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 import numpy as np
-import os,cv2,glob
-
-model = tf.keras.models.load_model('COVID-19-Detector.h5')
-class_list = ['Normal','COVID-19','Bacterial Pneumonia']
+import os,glob
 
 
-def preprocess(file_path):
+def preprocess(file_path,choice,model_path):
+    print("Getting you the results. Hold on...\n")
     images,links=[],[]
+    if choice=='Binary':
+        class_list = ['COVID-19','Normal']
+        model = tf.keras.models.load_model(model_path)
+    else:
+        class_list = ['Normal','COVID-19','Bacterial Pneumonia']
+        model = tf.keras.models.load_model(model_path)
+    print("Model loaded. Running Inference now...\n")
     for i in glob.glob(file_path+'/*'):
       img = open(i,'rb').read()
       img = tf.io.decode_jpeg(img,channels=1)
@@ -18,8 +23,11 @@ def preprocess(file_path):
     images = [class_list[i[0]] for i in images]
     for i,j in zip(images,links):
         print(os.path.basename(j).split(r'\\'),i)
+    print("\nDone!")
 
 
 if __name__=="__main__":
+    choice = str(input("Enter whether you want to do Binary or Multi-Label Classification: "))
+    model_path = glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)),str(choice))+'\\*.h5')
     path = input("Enter path containing X-ray files: ")
-    preprocess(path)
+    preprocess(path,choice,model_path[0])
